2f9687c37ba64f2c64dd6f343dbe828154c166a2
Martyna Bartniczak <aszkee@gmail.com>
2019-08-02 10:04:48 +0200

    client part

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
+
+const App= () => {
+    return(
+        <>
+            <BrowserRouter>
+           
+                    <Route />
+               
+            </BrowserRouter>
+        </>
+    )
+}
+
+export default App
