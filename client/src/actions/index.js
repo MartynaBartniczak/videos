097c392aee4f7a0b8da5cd693e1fad68a80377ec
Martyna Bartniczak <aszkee@gmail.com>
2019-08-12 14:33:55 +0200

    rendering list

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,7 +7,7 @@ import { SIGN_IN,
     DELETE_STREAM, 
     EDIT_STREAM 
 } from './types'
-import { async } from 'q';
+
 
 export const signIn = userId => {
     return {
@@ -23,24 +23,27 @@ export const signOut = () => {
 }
 
 export const createStream = formValues => async dispatch => {
-    const response = streams.post('/streams', formValues)
-    dispatch({type: CREATE_STREAM, payload: response.data})
+  const response = await streams.post('/streams', formValues)
+  dispatch({type: CREATE_STREAM, payload: response.data})
 }
 
 export const fetchStreams = () => async dispatch => {
-    const response = streams.get('./streams')
-    dispatch({type: FETCH_STREAMS, payload: response.data})
-}
+    const response = await streams.get('/streams');
+    dispatch({
+        type: FETCH_STREAMS, 
+        payload: response.data
+    });
+};
 
 export const fetchStream = id => async dispatch => {
-    const response = streams.get(`./streams/${id}`)
+    const response = streams.get(`/streams/${id}`)
     dispatch({type: FETCH_STREAM, payload: response.data})
 }
 export const editStream = (id, formValues) => async dispatch => {
-    const response = streams.post(`./streams/${id}`, formValues)
+    const response = streams.post(`/streams/${id}`, formValues)
     dispatch({type: EDIT_STREAM, payload: response.data})
 }
 export const deleteStream = id => async dispatch => {
-  await streams.delete(`./streams/${id}`)
+  await streams.delete(`/streams/${id}`)
   dispatch({type: DELETE_STREAM, payload: id})
 }
--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,14 @@ import reduxThunk from 'redux-thunk'
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducers,
-    composeEnhancers( applyMiddleware(reduxThunk)))
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
-<Provider store={store}>
-<App />
-</Provider>,
- document.querySelector('#root'))
+    <Provider store={store}>
+        <App  />
+    </Provider>,
+    document.querySelector('#root')
+);
